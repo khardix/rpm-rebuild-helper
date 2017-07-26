6d254cd4046ce3d7376f19eb7cf633f1745be809
Jan Staněk <jstanek@redhat.com>
2017-07-26 12:13:16 +0200

    Set up the packaging environment

--- README.md
+++
@@ -1,2 +0,0 @@
-# rpm-rebuild-helper
-An automation tool for rebuilding RPMs with focus on Software Collections
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from setuptools import setup, find_packages
+
+METADATA = {
+    'name': 'rpmrh',
+    'use_scm_version': True,
+    'description':
+        'An automation tool for rebuilding RPMs and Software Collections',
+    'long_description':
+        Path(__file__).with_name('README.rst').read_text('utf-8'),
+    'url':
+        'https://github.com/khardix/rpm-rebuild-helper',
+    'author': 'Jan Staněk',
+    'author_email': 'jstanek@redhat.com',
+
+    'license': 'GPLv3+',
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa: E501
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    'keywords': 'rpm scl softwarecollections rebuilding'
+}
+
+DEPENDENCIES = [
+    'click',
+]
+
+TEST_DEPENDENCIES = [
+    'pytest',
+]
+
+SETUP_DEPENDENCIES = [
+    'setuptools_scm',
+    'pytest-runner>=2.0,<3dev',
+]
+
+
+setup(
+    **METADATA,
+    setup_requires=SETUP_DEPENDENCIES,
+    install_requires=DEPENDENCIES,
+    tests_require=TEST_DEPENDENCIES,
+    packages=find_packages(exclude={'tests', 'docs'}),
+)
