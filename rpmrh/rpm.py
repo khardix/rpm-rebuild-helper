1f44f1c628db3ff1a075dd5435c7bc5620a248f1
Jan Staněk <jstanek@redhat.com>
2017-07-31 13:05:21 +0200

    Revert "Add interface to local RPMs"
    This reverts commit 62a88d442b561c429af632402836dabfde9341b5.

--- rpmrh/rpm.py
+++ rpmrh/rpm.py
@@ -1,9 +1,7 @@
 """RPM-related classes and procedures."""
 
 import operator
-import os
 from functools import partialmethod
-from pathlib import Path
 from typing import Callable, Tuple, TypeVar, Union
 
 import attr
@@ -83,85 +81,3 @@ class Metadata:
     __le__ = partialmethod(_compare, oper=operator.le)
     __gt__ = partialmethod(_compare, oper=operator.gt)
     __ge__ = partialmethod(_compare, oper=operator.ge)
-
-    def __str__(self):
-        """Pretty-print in the full NEVRA form."""
-
-        return self.nevra
-
-
-@attr.s(slots=True, cmp=False, frozen=True, init=False)
-class LocalFile(Metadata):
-    """RPM file on a local file system.
-
-    Note that after the initial creation, the metadata and the file name
-    are NOT kept in sync. This mirrors the fact that renaming the RPM
-    file will not change the metadata within.
-    """
-
-    #: Full path to the local file
-    path = attr.ib(validator=instance_of(Path), default=Path.cwd())
-
-    def __init__(self, full_path: Path) -> 'LocalFile':
-        """Construct the LocalFile from full path.
-
-        Keyword arguments:
-            full_path: Path to the file to represent as LocalFile.
-
-        Returns:
-            New initialized instance.
-        """
-
-        header = _read_rpm_header(full_path)
-        arguments = {
-            'name': header[_rpm.RPMTAG_NAME].decode('utf-8'),
-            'version': header[_rpm.RPMTAG_VERSION].decode('utf-8'),
-            'release': header[_rpm.RPMTAG_RELEASE].decode('utf-8'),
-            'epoch': header[_rpm.RPMTAG_EPOCHNUM],
-        }
-
-        # the header reports binary architecture on source RPMs
-        if header[_rpm.RPMTAG_SOURCEPACKAGE]:
-            arguments['arch'] = 'src'
-        else:
-            arguments['arch'] = header[_rpm.RPMTAG_ARCH]
-
-        super(LocalFile, self).__init__(**arguments)
-        # Subvert frozen object
-        object.__setattr__(self, 'path', full_path)
-
-        attr.validate(self)
-
-    def __str__(self):
-        """Pretty-print the absolute path"""
-
-        return str(self.path.resolve())
-
-    @property
-    def header(self) -> _rpm.hdr:
-        """Raw RPM header"""
-
-        return _read_rpm_header(self.path)
-
-
-# Private helper functions
-
-def _read_rpm_header(path: Path) -> _rpm.hdr:
-    """Extract a header from an RPM file.
-
-    Keyword arguments:
-        path: Path to an RPM file to read.
-
-    Returns:
-        Header of the RPM file.
-    """
-
-    ts = _rpm.TransactionSet()
-    # Ignore missing signatures
-    ts.setVSFlags(_rpm._RPMVSF_NOSIGNATURES)
-
-    fd = os.open(str(path), os.O_RDONLY)  # need file descriptor :(
-    try:
-        return ts.hdrFromFdno(fd)
-    finally:
-        os.close(fd)
