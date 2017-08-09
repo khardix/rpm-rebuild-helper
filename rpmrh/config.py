b2a05d79d0287f4515ecbe68df6aad624dfab964
Jan Staněk <jstanek@redhat.com>
2017-08-09 18:03:57 +0200

    Implement simple config file loading

--- rpmrh/config.py
+++ rpmrh/config.py
@@ -1,16 +1,24 @@
 """Configuration file processing"""
 
-from typing import Mapping, Callable, Optional, Type
+# TODO: Config file schema and validation
+
+from pathlib import Path
+from typing import Mapping, Callable, Optional, Type, Any, TextIO, Sequence
+
+import toml
+
+# Expected type of the type registry
+TypeRegistry = Mapping[str, Callable]
 
 #: Registry of all the types that can be created from configuration
-TYPE_REGISTRY: Mapping[str, Callable] = {}
+KNOWN_TYPES: TypeRegistry = {}
 
 
 def register_type(
     name: str,
     initializer: Optional[str] = None,
     *,
-    registry: Mapping[str, Callable] = TYPE_REGISTRY
+    registry: TypeRegistry = KNOWN_TYPES
 ) -> Callable:
     """Enable a type to be used as a configured source.
 
@@ -41,3 +49,57 @@ def register_type(
 
         return cls
     return decorator
+
+
+def load_source(
+    configuration: Mapping,
+    *,
+    registry: TypeRegistry = KNOWN_TYPES
+) -> Any:
+    """Load single source interface from its configuration.
+
+    Keyword arguments:
+        configuration: The raw configuration values for the source.
+        registry: The type registry to get initializers from.
+
+    Returns:
+        New instance of the source interface.
+    """
+
+    type_name = configuration.pop('type')
+    return registry[type_name](**configuration)
+
+
+def load_config_file(file: TextIO, *, registry: TypeRegistry = KNOWN_TYPES):
+    """Load and interpret a single configuration file.
+
+    Keyword arguments:
+        file: The open file to read configuration from.
+        registry: The type registry to get initializers from.
+    """
+
+    configuration = toml.load(file)
+
+    for source in configuration['source']:
+        instance = load_source(source, registry=registry)
+        instance.register()
+
+
+def load_configuration(
+    path_seq: Sequence[Path],
+    *,
+    registry: TypeRegistry = KNOWN_TYPES
+):
+    """Load configuration from the files in path_seq.
+
+    The sequence represents priority: values from later files will overwrite
+    earlier ones, if conflicts arise.
+
+    Keyword arguments:
+        path_seq: Paths to configuration files to process.
+        registry: The type registry to get initializers from.
+    """
+
+    for path in path_seq:
+        with path.open(mode='r', encoding='utf-8') as istream:
+            load_config_file(istream, registry=registry)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,10 @@
 """Tests for the configuration mechanism"""
 
+from io import StringIO
+from pathlib import Path
+
 import pytest
+import toml
 
 from rpmrh import config
 
@@ -10,6 +14,51 @@ def registry():
     """Fresh configuration registry"""
 
     return dict()
+
+
+@pytest.fixture
+def source_type(registry):
+    """Registered test source type."""
+
+    @config.register_type('test', registry=registry)
+    class Tested(dict):
+        instance_ids = set()
+
+        def register(self):
+            Tested.instance_ids.add(id(self))
+
+    return Tested
+
+
+@pytest.fixture
+def source_configuration(source_type):
+    """Raw configuration for the test source."""
+
+    return {
+        'type': 'test',
+        'scalar': 42,
+        'sequence': ['Hello', 'World'],
+        'mapping': {'lang': 'en_US'},
+    }
+
+
+@pytest.fixture
+def config_file(source_configuration):
+    """Single open configuration file."""
+
+    contents = toml.dumps({'source': [source_configuration]})
+    return StringIO(contents)
+
+
+@pytest.fixture
+def config_path_sequence(tmpdir, config_file):
+    """Sequence of configuration file paths."""
+
+    path = Path(str(tmpdir), 'test-source.toml')
+    with path.open(mode='w', encoding='utf-8') as ostream:
+        ostream.write(config_file.read())
+
+    return [path]
 
 
 def test_register_simple(registry):
@@ -59,3 +108,40 @@ def test_double_registration_fails(registry):
         @config.register_type('test', registry=registry)
         class B:
             pass
+
+
+def test_single_source_is_instantiated(
+    registry,
+    source_type,
+    source_configuration
+):
+    """Single source configuration is properly interpreted."""
+
+    instance = config.load_source(source_configuration, registry=registry)
+
+    assert isinstance(instance, source_type)
+    assert 'type' not in instance
+    assert all(
+        instance[i] == source_configuration[i]
+        for i in ('scalar', 'sequence', 'mapping')
+    ), instance
+
+
+def test_config_file_is_interpreted(registry, source_type, config_file):
+    """Configuration file is properly interpreted."""
+
+    config.load_config_file(config_file, registry=registry)
+
+    assert len(source_type.instance_ids) == 1
+
+
+def test_config_paths_are_interpreted(
+    registry,
+    source_type,
+    config_path_sequence
+):
+    """Sequence of configuration files is interpreted properly."""
+
+    config.load_configuration(config_path_sequence, registry=registry)
+
+    assert len(source_type.instance_ids) == 1
