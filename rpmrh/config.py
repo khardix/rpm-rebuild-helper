a90ef326669a8619b371787a223c98c4619d8d05
Jan Staněk <jstanek@redhat.com>
2017-08-28 12:37:57 +0200

    Implement basic configuration context object

--- rpmrh/config.py
+++ rpmrh/config.py
@@ -1,15 +1,21 @@
 """Configuration file processing"""
 
-from typing import Mapping
+from functools import partial
+from typing import Mapping, Any
 
 import attr
 import cerberus
 import click
+from attr.validators import instance_of
+from pytrie import StringTrie
+
+from .service.configuration import InitializerMap, INIT_REGISTRY
 
 
 #: Configuration file schema
 SCHEMA = {
-    'service': {  # list of services
+    'service': {'rename': 'services'},  # allow both singular and plural forms
+    'services': {  # list of services
         'type': 'list',
         'schema': {  # single service
             'type': 'dict',
@@ -37,19 +43,103 @@ class ConfigurationError(click.ClickException):
         ])
 
 
+def _validate_raw_values(config_map: Mapping) -> Mapping:
+    """Ensure that the raw configuration map upholds the configuration schema.
+
+    Keyword arguments:
+        config_map: The mapping to validate.
+
+    Returns:
+        Validated and normalized configuration.
+
+    Raises:
+        ConfigurationError: Invalid configuration map.
+    """
+
+    validator = cerberus.Validator(schema=SCHEMA)
+
+    if validator.validate(config_map):
+        return validator.document
+
+    else:
+        message = 'Invalid configuration'
+        raise ConfigurationError(message, validator.errors)
+
+
+def _configure_service(
+    config_map: Mapping,
+    *,
+    registry: InitializerMap = INIT_REGISTRY
+):
+    """Create a new service instance from its configuration.
+
+    Keyword arguments:
+        config_map: The configuration values for a service.
+        registry: The initializer registry to use.
+
+    Returns:
+        Configured service instance.
+    """
+
+    service_type = config_map.pop('type')
+    return registry[service_type](**config_map)
+
+
+@attr.s(slots=True, frozen=True)
+class ServiceIndex:
+    """Container for fast retrieval of marked/tagged services."""
+
+    #: Service attribute that holds the keys to index by
+    key_attribute = attr.ib(validator=instance_of(str))
+
+    #: The indexed services themselves
+    container = attr.ib(init=False, default=attr.Factory(StringTrie))
+
+    def insert(self, service: Any) -> None:
+        """Index a new service."""
+
+        key_set = getattr(service, self.key_attribute, frozenset())
+        for key in key_set:
+            self.container[key] = service
+
+
 @attr.s(slots=True, frozen=True)
 class Context:
     """Application configuration context."""
 
-    #: Raw values for validation and further processing
-    _raw_config_map = attr.ib()
+    #: Configured services by tag
+    tag_index = attr.ib(
+        validator=instance_of(ServiceIndex),
+        default=lambda _self: ServiceIndex(key_attribute='tag_prefixes')
+    )
 
-    @_raw_config_map.validator
-    def validate_config_map(self, _attribute: attr.Attribute, value: Mapping):
-        """Ensure that the raw config map upholds the configuration schema."""
+    @classmethod
+    def from_mapping(
+        cls,
+        config_map: Mapping,
+        *,
+        service_registry: InitializerMap = INIT_REGISTRY
+    ):
+        """Create new Context from raw configuration mapping.
 
-        validator = cerberus.Validator(schema=SCHEMA)
+        Keyword arguments:
+            config_map: The values to create the instance from.
+            service_registry: Initializer registry for the configured services.
 
-        if not validator.validate(value):
-            message = 'Invalid configuration'
-            raise ConfigurationError(message, validator._errors)
+        Returns:
+            Initialized Context.
+        """
+
+        config_map = _validate_raw_values(config_map)
+        configure_service = partial(
+            _configure_service,
+            registry=service_registry
+        )
+
+        tag_index = ServiceIndex(key_attribute='tag_prefixes')
+
+        # Instantiate and index the services
+        for service in map(configure_service, config_map['services']):
+            tag_index.insert(service)
+
+        return cls(tag_index=tag_index)
--- tests/test_config.py
+++ tests/test_config.py
@@ -54,15 +54,86 @@ def invalid_configuration(valid_configuration):
     return configuration
 
 
-def test_context_validation_ok(valid_configuration):
-    """Valid configuration is validated properly."""
+@pytest.fixture
+def configured_service(service_type, valid_configuration, registry):
+    """Configured instance of the test service."""
 
-    ctx = config.Context(valid_configuration)
-    assert ctx._raw_config_map
+    configuration = deepcopy(valid_configuration['service'][0])
+    return config._configure_service(configuration, registry=registry)
 
 
-def test_context_validation_fail(invalid_configuration):
-    """Invalid configuration is reported."""
+@pytest.fixture
+def tag_index():
+    """Index structure for tag_prefixes."""
+
+    return config.ServiceIndex(key_attribute='tag_prefixes')
+
+
+@pytest.fixture
+def other_index():
+    """Index structure for different key attribute than tag_prefixes."""
+
+    return config.ServiceIndex(key_attribute='other_prefixes')
+
+
+def test_valid_configuration_validates(valid_configuration):
+    """Valid configuration passes the validation."""
+
+    assert config._validate_raw_values(valid_configuration)
+
+
+def test_invalid_configuration_raises(invalid_configuration):
+    """Invalid configuration raises ConfigurationError on validation."""
 
     with pytest.raises(config.ConfigurationError):
-        config.Context(invalid_configuration)
+        config._validate_raw_values(invalid_configuration)
+
+
+def test_service_is_configured(service_type, configured_service):
+    """Registered service is correctly configured."""
+
+    assert isinstance(configured_service, service_type)
+    assert configured_service.keys() == {'scalar', 'sequence', 'mapping'}
+
+
+def test_relevant_service_is_indexed(configured_service, tag_index):
+    """Service with proper attribute is inserted into the index."""
+
+    tag_index.insert(configured_service)
+
+    assert all(
+        prefix in tag_index.container
+        for prefix in configured_service.tag_prefixes
+    )
+
+
+def test_irrelevant_service_pass_index(configured_service, other_index):
+    """Service without the attribute is skipped without an exception."""
+
+    other_index.insert(configured_service)
+
+    assert not any(
+        prefix in other_index.container
+        for prefix in configured_service.tag_prefixes
+    )
+
+
+def test_contex_is_created_from_sigle_mapping(
+    valid_configuration,
+    configured_service,
+    registry
+):
+    """Configuration context can be created from a single mapping."""
+
+    print(valid_configuration)
+
+    context = config.Context.from_mapping(
+        valid_configuration,
+        service_registry=registry,
+    )
+
+    assert context
+    assert all(
+        prefix in context.tag_index.container
+        for prefix in configured_service.tag_prefixes
+    )
