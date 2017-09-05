55f617fedb9831fda6e49747243bbafe0f663266
Jan Staněk <jstanek@redhat.com>
2017-09-05 11:50:33 +0200

    Add indirect service instantiation and service index

--- rpmrh/service/configuration.py
+++ rpmrh/service/configuration.py
@@ -4,7 +4,11 @@ The registered callables will be used to construct relevant instances
 from the application configuration files.
 """
 
-from typing import Mapping, Callable, Optional, Type
+from typing import Mapping, Callable, Optional, Type, Any, Iterator
+
+import attr
+from attr.validators import instance_of
+from pytrie import StringTrie
 
 # Type of service initializer table
 InitializerMap = Mapping[str, Callable]
@@ -47,3 +51,70 @@ def register(
 
         return cls
     return decorator
+
+
+def instantiate(
+    service_conf_map: Mapping,
+    *,
+    registry: InitializerMap = INIT_REGISTRY
+) -> Any:
+    """Create an instance of registered type from its configuration.
+
+    Keyword arguments:
+        service_conf_map: Configuration for the service instance.
+        registry: The registry to retrieve the initializer from.
+
+    Returns:
+        New instance of configured service.
+
+    Raises:
+        KeyError: Configuration does not specify service type.
+        KeyError: Unknown service type.
+    """
+
+    type_name = service_conf_map.pop('type')
+    return registry[type_name](**service_conf_map)
+
+
+@attr.s(slots=True, frozen=True)
+class Index(Mapping):  # add mixin methods
+    """Service instance index."""
+
+    # The actual container for the instances
+    _container = attr.ib(init=False, default=attr.Factory(StringTrie))
+
+    #: Name of the attribute to index by.
+    key_attribute = attr.ib(validator=instance_of(str))
+
+    def insert(self, service: Any) -> Any:
+        """Index a service by the matching attribute.
+
+        Keyword arguments:
+            service: The service to index.
+
+        Returns:
+            The service passed as argument.
+        """
+
+        key_set = getattr(service, self.key_attribute, frozenset())
+        for key in key_set:
+            self._container[key] = service
+
+        return service
+
+    # Mapping interface
+
+    def __getitem__(self, key: str) -> Any:
+        """Get item with longest prefix match to a key."""
+
+        return self._container.longest_prefix_value(key)
+
+    def __iter__(self) -> Iterator[str]:
+        """Iterate over the contained keys."""
+
+        return iter(self._container)
+
+    def __len__(self) -> int:
+        """Report the size of container."""
+
+        return len(self._container)
--- tests/service/test_configuration.py
+++ tests/service/test_configuration.py
@@ -1,8 +1,26 @@
 """Tests for the service configuration mechanism"""
 
+from collections import namedtuple
+
 import pytest
 
 from rpmrh.service import configuration
+
+
+class Registered:
+    """Test class for registration"""
+
+    # Name in the registries
+    type_name = 'registered'
+
+    def __init__(self, name):
+        self.name = name
+
+
+# configurations for Registered instance
+registered_configuration = pytest.mark.parametrize('service_configuration', [
+    {'type': Registered.type_name, 'name': 'configured'},
+])
 
 
 @pytest.fixture
@@ -12,20 +30,32 @@ def registry():
     return dict()
 
 
-def test_register_simple(registry):
+@pytest.fixture
+def filled_registry(registry):
+    """Configuration registry with expected contents"""
+
+    configuration.register(Registered.type_name, registry=registry)(Registered)
+
+    assert Registered.type_name in registry
+    assert registry[Registered.type_name] is Registered
+
+    return registry
+
+
+@pytest.fixture
+def service_index():
+    """Empty service index"""
+
+    return configuration.Index('test_key_set')
+
+
+def test_register_simple(filled_registry):
     """Class using __init__ to configure can be registered."""
 
-    @configuration.register('test', registry=registry)
-    class Test:
-        def __init__(self, identification):
-            self.identification = identification
+    instance = filled_registry[Registered.type_name]('test')
 
-    assert 'test' in registry
-
-    instance = registry['test']('registered')
-
-    assert isinstance(instance, Test)
-    assert instance.identification == 'registered'
+    assert isinstance(instance, Registered)
+    assert instance.name == 'test'
 
 
 def test_register_custom_initializer(registry):
@@ -68,3 +98,48 @@ def test_invalid_initializer_fails(registry):
         @configuration.register('test', initializer='none', registry=registry)
         class Test:
             pass
+
+
+@registered_configuration
+def test_instantiate_make_instance(service_configuration, filled_registry):
+    """Registered type can be instantiated indirectly."""
+
+    instance = configuration.instantiate(
+        service_configuration,
+        registry=filled_registry,
+    )
+
+    assert instance
+    assert isinstance(instance, Registered)
+    assert instance.name == service_configuration['name']
+
+
+@registered_configuration
+def test_instantiate_raises_unknown(service_configuration, registry):
+    """Exception is raised on unknown type."""
+
+    with pytest.raises(KeyError):
+        configuration.instantiate(service_configuration, registry=registry)
+
+
+def test_index_inserts_matched(service_index):
+    """Matching service is indexed."""
+
+    matching = namedtuple('Matching', ['test_key_set'])({'test', 'key'})
+
+    inserted = service_index.insert(matching)
+
+    assert inserted is matching
+    assert all(key in service_index for key in matching.test_key_set)
+    assert service_index[matching.test_key_set.pop()] is matching
+
+
+def test_index_pass_unmatched(service_index):
+    """Mismatching service is passed without exception."""
+
+    mismatching = namedtuple('Mismatching', ['key_set'])({'test', 'key'})
+
+    passed = service_index.insert(mismatching)
+
+    assert passed is mismatching
+    assert all(key not in service_index for key in mismatching.key_set)
