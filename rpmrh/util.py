247e3d09d238f85d4b026ac7c558e4c80366d7e7
Jan Staněk <jstanek@redhat.com>
2017-09-06 15:38:44 +0200

    Add utility functions for opening configuration

--- rpmrh/util.py
+++ rpmrh/util.py
@@ -1,12 +1,18 @@
 """Miscellaneous utility functions."""
 
 from importlib import import_module
+from itertools import chain
+from io import TextIOWrapper
 from operator import attrgetter
-from typing import Any, Optional
+from pathlib import Path
+from pkg_resources import resource_listdir, resource_stream
+from typing import Any, Optional, Iterator
+from typing import TextIO
 
 import requests
 from click import ClickException
 from requests_file import FileAdapter
+from xdg.BaseDirectory import load_config_paths
 
 
 class SystemImportError(ClickException):
@@ -85,3 +91,61 @@ def default_requests_session(session: Optional[requests.Session] = None):
     session.mount('file://', FileAdapter())
 
     return session
+
+
+def open_resource_files(
+    root_dir: str,
+    extension: str,
+    *,
+    encoding: str = 'utf-8',
+    package: str = __package__
+) -> Iterator[TextIO]:
+    """Open package resources text files.
+
+    Keyword arguments:
+        root_dir: Path to the resource directory containing the files.
+        extension: Extension of the files that should be opened.
+        encoding: File encoding.
+        package: The namespace to look for the resources in.
+
+    Yields:
+        Opened text streams.
+    """
+
+    base_names = resource_listdir(package, root_dir)
+    paths = (
+        '/'.join((root_dir, name))
+        for name in base_names
+        if name.endswith(extension)
+    )
+    binary_streams = (resource_stream(package, p) for p in paths)
+    text_streams = (
+        TextIOWrapper(bs, encoding=encoding)
+        for bs in binary_streams
+    )
+
+    yield from text_streams
+
+
+def open_config_files(
+    extension: str,
+    *,
+    encoding: str = 'utf-8'
+) -> Iterator[TextIO]:
+    """Open user configuration files.
+
+    Keyword arguments:
+        extension: Extension of the files that should be opened.
+        encoding: File encoding.
+
+    Yields:
+        Opened text streams.
+    """
+
+    conf_dirs = map(Path, load_config_paths(__package__))
+    conf_file_paths = chain.from_iterable(
+        pth.glob('*{}'.format(extension)) for pth in conf_dirs
+    )
+    conf_files = (pth.open(encoding=encoding) for pth in conf_file_paths)
+
+    yield from conf_files
