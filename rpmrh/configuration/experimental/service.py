6635a83b21a5d52c26451041494cdc1bc7e2d9a4
Jan Staněk <jstanek@redhat.com>
2018-01-05 14:26:17 +0100

    Implement dynamic type registration for services

--- rpmrh/configuration/experimental/service.py
+++ rpmrh/configuration/experimental/service.py
@@ -15,9 +15,77 @@ SCHEMA = {'service': {  # dummy key to allow for top-level structure validation
     }},
 }}
 
+#: Registered service types
+KNOWN_TYPES = {}
+
+
+class DuplicateError(KeyError):
+    """Key already present in a dictionary."""
+
 
 class InvalidConfiguration(Exception):
     """The service configuration is not valid."""
+
+
+# Dynamic configuration type registration
+
+def register(
+    name: str,
+    initializer: Optional[str] = None,
+    *,
+    registry: MutableMapping = KNOWN_TYPES,
+) -> Callable[[Type], Type]:
+    """Register an object initializer for service class.
+
+    Keyword arguments:
+        name: The name of the registered class within configuration files.
+        initializer: Name of the callable to use as initializer value.
+            If None, __init__ will be used.
+        registry: The mapping to insert the initializer into.
+
+    Returns:
+        Decorator for the class to be registered.
+
+    Raises:
+        DuplicateError: A type with this name is already registered.
+    """
+
+    if name in registry:
+        raise DuplicateError(name)
+
+    def decorator(cls: Type) -> Type:
+        """Insert the type in the registry."""
+
+        if initializer:
+            registry[name] = getattr(cls, initializer)
+        else:
+            registry[name] = cls
+
+        return cls
+    return decorator
+
+
+def make_instance(
+    configuration_map: MutableMapping,
+    *,
+    registry: MutableMapping = KNOWN_TYPES,
+):
+    """Turn configuration into proper instance.
+
+    Keyword arguments:
+        configuration_map: The configuration for the instance.
+        registry: The mapping that contains registered initializers.
+
+    Returns:
+        Configured instance.
+
+    Raises:
+        KeyError: configuration_map is missing 'type' key.
+        KeyError: Requested type is missing from registry.
+    """
+
+    type_name = configuration_map.pop('type')
+    return registry[type_name](**configuration_map)
 
 
 # Configuration file processing
