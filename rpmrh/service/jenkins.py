0c45b69a87f46b02ebec324c699ef404d2e80f33
Jan Staněk <jstanek@redhat.com>
2018-02-01 16:49:11 +0100

    Implement log line parsing

--- rpmrh/service/jenkins.py
+++ rpmrh/service/jenkins.py
@@ -1,10 +1,48 @@
 """Jenkins test runner integration"""
 
+import re
+
 import attr
 import jenkins
 from attr.validators import instance_of
 
+from .. import rpm
 from ..configuration import service
+
+#: RE of package lines in YUM/DNF logs
+PKG_LINE_RE = re.compile('''
+    (?P<name>\S+)            # package name
+    (?:\.(?P<arch>\w+))     # package architecture
+    \s+                     # white-space separator
+    (?P<epoch>\d+):         # required package epoch
+    (?P<version>[\w.]+)-    # package version
+    (?P<release>\w+(?:\.[\w+]+)+)  # package release, with required dist tag
+''', flags=re.VERBOSE)
+
+
+def _parse_package_line(line: str) -> rpm.Metadata:
+    """Parse a DNF log line with package information.
+
+    The expected format of the line is::
+
+        {name}.{arch} {epoch}:{version}-{release}
+
+    Keyword arguments:
+        line: The line to parse.
+
+    Returns:
+        The parsed metadata.
+
+    Raises:
+        ValueError: The line is not in the expected format.
+    """
+
+    match = PKG_LINE_RE.search(line)
+
+    if not match:
+        raise ValueError('Unexpected log line: ' + line)
+
+    return rpm.Metadata(**match.groupdict())
 
 
 @service.register('jenkins')
@@ -14,9 +52,6 @@ class Server:
 
     #: Base URL of the server
     url = attr.ib(validator=instance_of(str))
-
-    #: Format of the job name
-    job_name_format = attr.ib(validator=instance_of(str))
 
     #: API handle for low-level calls
     _handle = attr.ib(validator=instance_of(jenkins.Jenkins))
--- tests/service/test_jenkins.py
+++ tests/service/test_jenkins.py
@@ -1,25 +1,39 @@
+from types import MappingProxyType
+
 import jenkins
 import pytest
 
-from rpmrh import service
+from rpmrh import service, rpm
 
 #: Build containing install-all-pkgs artifact
-ALL_PKGS_URL = 'https://ci.centos.org/job/SCLo-pkg-rh-java-common-rh-C7-candidate-x86_64/'  # noqa: E501
+ALL_PKGS = MappingProxyType({
+    'url': 'https://ci.centos.org/job/SCLo-pkg-rh-java-common-rh-C7-candidate-x86_64/',  # noqa: E501
+    'name': 'SCLo-pkg-rh-java-common-rh-C7-candidate-x86_64',
+    'format': MappingProxyType({'collection': 'rh-java-common', 'el': 7}),
+})
 #: Build containing only install artifact
-INSTALL_ONLY_URL = 'https://ci.centos.org/job/SCLo-pkg-devtoolset-7-rh-C7-buildlogs-x86_64/'  # noqa: E501
+INSTALL_ONLY = MappingProxyType({
+    'url': 'https://ci.centos.org/job/SCLo-pkg-devtoolset-7-rh-C7-buildlogs-x86_64/',  # noqa: E501
+    'name': 'SCLo-pkg-devtoolset-7-rh-C7-buildlogs-x86_64',
+    'format': MappingProxyType({'collection': 'devtoolset-7', 'el': 7}),
+})
 #: Build contains multiple package listings in install artifact
-MULTIPLE_SECTION_URL = 'https://ci.centos.org/job/SCLo-pkg-devtoolset-7-rh-C7-buildlogs-x86_64/'  # noqa: E501
+MULTIPLE_SECTION = INSTALL_ONLY
+
+# Parametrization sequences
+ALL_JOBS = ALL_PKGS, INSTALL_ONLY
 
 
 @pytest.fixture
 def server(mocker):
     url = 'https://ci.centos.org/'
-    job_fmt = 'SCLo-pkg-devtoolset-7-rh-C7-buildlogs-x86_64'
     handle = mocker.Mock(spec=jenkins.Jenkins(url))
+
+    handle.get_job_info.side_effect = \
+        lambda name: {j['name']: j for j in ALL_JOBS}[name]
 
     return service.jenkins.Server(
         url=url,
-        job_name_format=job_fmt,
         handle=handle,
     )
 
@@ -28,3 +42,43 @@ def test_server_creation(server):
     """Server instance can be created"""
 
     assert server
+
+
+@pytest.mark.parametrize('line,expected_metadata', [
+    (
+        'java-1.7.0-openjdk.x86_64 1:1.7.0.161-2.6.12.0.el7_4',
+        rpm.Metadata(
+            name='java-1.7.0-openjdk',
+            epoch=1,
+            version='1.7.0.161',
+            release='2.6.12.0.el7_4',
+            arch='x86_64',
+        ),
+    ),
+    (
+        'maven30-maven-artifact.noarch 0:2.2.1-47.11.el7',
+        rpm.Metadata(
+            name='maven30-maven-artifact',
+            epoch=0,
+            version='2.2.1',
+            release='47.11.el7',
+            arch='noarch',
+        ),
+    ),
+])
+def test_parse_package_line_accepts_correct_line(line, expected_metadata):
+    """YUM/DNF package line parsing accepts correct lines"""
+
+    assert service.jenkins._parse_package_line(line) == expected_metadata
+
+
+@pytest.mark.parametrize('line', [
+    '',
+    'Dependency Installed:',
+    'Installing : 1:perl-parent-0.225-244.el7.noarch        1/595',
+])
+def test_parse_package_line_reports_unexpected_line(line):
+    """YUM/DNF package line parsing raises on unexpected line"""
+
+    with pytest.raises(ValueError):
+        service.jenkins._parse_package_line(line)
