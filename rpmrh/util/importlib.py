d1f8df42c910d7b48d6770d1c6ad100932617d36
Jan Staněk <jstanek@redhat.com>
2019-02-28 13:12:47 +0100

    Let system_import always use system packages.
    `importlib.system_import` now acts as sort of "jail break"
    from the virtual environment, and always uses system-installed packages
    (even if a package of the same name is present in the virtual
    environment).

--- rpmrh/util/importlib.py
+++ rpmrh/util/importlib.py
@@ -1,8 +1,56 @@
 """Utilities related to the import system."""
 
+import sys
+from contextlib import ContextDecorator
 from importlib import import_module
 from operator import attrgetter
+from pathlib import Path
 from typing import Any
+
+
+class system_environment(ContextDecorator):
+    """Use only system site-packages, ignoring any virtual environments.
+
+    When within this context, the `sys.path` variable is modified
+    to ignore packages installed within a virtual environment
+    and to use the system (global) installed packages instead.
+
+    Use sparingly and with care.
+    """
+
+    #: Relative path from library directory to site-packages
+    _SITEDIR: Path = Path(
+        f"python{sys.version_info.major}.{sys.version_info.minor}", "site-packages"
+    )
+    #: Noarch library directory
+    _LIBRARY_NOARCH: Path = Path("lib")
+    #: Architecture-dependent library direcotry
+    _LIBRARY_ARCH: Path = Path("lib32" if sys.maxsize < 2 ** 32 else "lib64")
+
+    def __init__(self):
+        """Prepare path backup stack for this context."""
+        super().__init__()
+
+        self.path_stack = []
+
+    def __enter__(self):
+        """Replace paths of virtual environment with system ones in `sys.path`."""
+
+        self.path_stack.append(sys.path)
+
+        in_virtual_env = sys.prefix != sys.base_prefix
+        if in_virtual_env:
+            # Strip paths of virtual environment from sys.path
+            modified = [pth for pth in sys.path if not pth.startswith(sys.prefix)]
+            # Append paths of system environment
+            modified.append(str(sys.base_prefix / self._LIBRARY_NOARCH / self._SITEDIR))
+            modified.append(str(sys.base_prefix / self._LIBRARY_ARCH / self._SITEDIR))
+            # Use the system environment
+            sys.path = modified
+
+    def __exit__(self, *_exc_info):
+        """Restore original `sys.path`"""
+        sys.path = self.path_stack.pop()
 
 
 class SystemImportError(ImportError):
@@ -18,6 +66,7 @@ class SystemImportError(ImportError):
         super().__init__("System Import Error: {!s}".format(user_msg))
 
 
+@system_environment()
 def system_import(module_name: str, *attribute_names) -> Any:
     """Try to import system-installed package, with user warning on failure.
 
